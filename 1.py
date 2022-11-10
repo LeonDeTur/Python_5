c646b24ed734d941cf56329e6b2a14ad034e3458
Leon Tur <tutrkov.leon-2@yandex.ru>
2022-11-10 21:12:44 +0300

    Finished Ex.1

--- 1.py
+++ 1.py
@@ -1,45 +1,34 @@
 # Напишите программу, удаляющую из текста все слова, содержащие ""абв"".
 
-test = 'Попал в просак баклан в будний день, что не был солнечным тогда.'
+test = (input('Введите текст на русском языке: '))
 
 def Find_ABC_in_text (text, index):
     for index in range(len(text)):
         if (text[index] == 'А') | (text[index] == 'а') | (text[index] == 'Б') | (text[index] == 'б') | (text[index] == 'В') | (text[index] == 'в'):
             return index
     return -1
+
 def Find_indexes_of_word (index_letter, text):
-    while (text[index_letter].isalpha()) and (index_letter > 0):
+    tmp = index_letter
+    while (1072 <= ord(text[index_letter]) <= 1103) and (index_letter > 0):
         index_letter -= 1
     index_start = index_letter
-    index_letter += 1
-    while text[index_letter].isalpha() & index_letter >= 0:
+    index_letter = tmp
+    while (1072 <= ord(text[index_letter]) <= 1103) and (index_letter >= 0):
         index_letter += 1
     index_end = index_letter
 
     return [index_start, index_end]
 
-def Fill_with_empty (text, indexes_list):
-    text_list = list(text)
-    _ = indexes_list[1]
-    while _ in range(indexes_list[1], indexes_list[0]):
-        text_list.pop(_)
-        indexes_list[0] -= 1
-    text = ''.join(text_list)
-    return text
-
 def Clean_text (text, index):
-    if index != len(text) - 1:
-        index = Find_ABC_in_text (text, 0)
-        if index == -1:
-            return text
+    while index != -1:
+        index = Find_ABC_in_text (text, index)
         tmp_list = Find_indexes_of_word(index, text)
-        Fill_with_empty(text, tmp_list)
+        text = text[:tmp_list[0]] + text[tmp_list[1]:]
         index = tmp_list[0]
-        Clean_text(text, index)
     return text
 
-print(Clean_text(test, 0))
-print(test[0])
+print('Текст, без слов с буквами а,б,в: ' + Clean_text(test, 0))
 
 
 
